fbe94cd7896b5ffd56caf0d0f0b6fcbe2569d371
Danil <danilsolom@mail.ru>
2020-09-22 20:52:51 +1000

    Start log fixes

--- libs/stream.ts
+++ libs/stream.ts
@@ -8,11 +8,14 @@ async function setStream(times: number, index: number): Promise<IResult[]> {
 	for (let i = 0; i < times; i++) {
 		const path = get.url(times * index);
 		const url = `https://prnt.sc/${path}`;
-		if (settings.log) console.log(url);
+		if (!settings.filterBadPaths) console.log(url);
 
 		const ulrResult = await parse(url);
-		if (ulrResult) result.push(ulrResult);
-		else i--;
+		if (ulrResult) {
+			result.push(ulrResult);
+			if (settings.log)
+				console.log(`Already ${~~(((i + 1) / times) * 100)}% in stream number ${index + 1}`);
+		} else i--;
 	}
 	return result;
 }
